6629dc28d37b303a6081670544111d0f33a8f94c
tancheanyong <tancheanyong@gmail.com>
2022-11-23 15:57:55 +0800

    feat: Sun texture

--- src/components/Sun.tsx
+++ src/components/Sun.tsx
@@ -1,11 +1,24 @@
-import { FC } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { FC, useRef } from "react";
+import { Mesh, TextureLoader } from "three";
 
 const Sun: FC<any> = () => {
+  const sunRef = useRef<Mesh>(null!);
+  const sunTexture = useLoader(TextureLoader, "8k_sun.jpg");
+
+  useFrame(({ clock }) => {
+    sunRef.current.rotation.y = clock.getElapsedTime() * 0.2;
+  });
+
   return (
-    <mesh position={[0, 0, 0]}>
+    <mesh position={[0, 0, 0]} ref={sunRef}>
       <pointLight />
       <sphereGeometry />
-      <meshPhysicalMaterial color={"yellow"} emissive={"yellow"} />
+      <meshPhysicalMaterial
+        emissiveMap={sunTexture}
+        emissive={"yellow"}
+        emissiveIntensity={0.8}
+      />
     </mesh>
   );
 };
